ff9d9b28d5e434eaabcef9f2e42f151021c85685
vitalii <2456vitalic@gmail.com>
2022-08-03 13:47:41 +0300

    Add Header with Materialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import Header from "./header/Header";
-
+import Header from "./components/header/Header";
 import 'material-icons/iconfont/material-icons.css';
-
-
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <>
       <Header/>
+        <Navbar/>
     </>
   );
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,21 +8,21 @@ const Header = () => {
             <ul id="dropdown1" className="dropdown-content">
                 <li><a href="#!">one</a></li>
                 <li><a href="#!">two</a></li>
-                <li className="divider"></li>
+                <li className="divider"/>
                 <li><a href="#!">three</a></li>
             </ul>
-            <nav>
-                <div className="nav-wrapper">
-                    <ul className="left hide-on-med-and-down">
-                        <div className='gorod'>
+            <nav className="header-wrapper">
+                <div >
+                    <ul className="left">
+                        <div className='city-header'>
                         <li>
-                            <a className="dropdown-trigger" href="#!" data-target="dropdown1">Saint Petersburg
-                            <i className="material-icons right">arrow_drop_down</i>
+                            <a  className="dropdown-trigger header-text" href="#!" data-target="dropdown1">Saint Petersburg
+                            <i className="material-icons right header-text">arrow_drop_down</i>
                         </a>
                         </li>
-                            <div className='zvonok'>
+                            <div className='call-header'>
                             <li>
-                                <a href="tel:84952592500">8 495 259 25 00</a>
+                                <a className="header-text" href="tel:84952592500">8 495 259 25 00</a>
                             </li>
                             </div>
                         </div>
