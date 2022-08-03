18fad9c103ba9b56d1190e270370e168517e6c07
vitalii <2456vitalic@gmail.com>
2022-08-03 15:23:02 +0300

    Add footer with Materialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <>
       <Header/>
         <Navbar/>
+        <Footer/>
     </>
   );
 }
