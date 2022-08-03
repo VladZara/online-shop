3fdc3f4b2f3fd717701175188d5059a06dbacffa
ArturTurcan <105306163+ArturTurcan@users.noreply.github.com>
2022-08-03 20:13:24 +0300

    add description block

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import FirstBlock from "./components/first_block/FirstBlock";
+import DescriptionBlock from "./components/descriptionBlock/DescriptionBlock";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Header/>
         <Navbar/>
       <FirstBlock/>
+        <DescriptionBlock/>
         <Footer/>
     </>
   );
