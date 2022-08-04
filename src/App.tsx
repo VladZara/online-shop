3db270d3e752d8e5751b0b357e866a073aaf88c0
vitalii <2456vitalic@gmail.com>
2022-08-04 10:52:53 +0300

    Rebase promotional blocks

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import SaleCard from "./components/promotional_blocks/SaleCard";
 
+
+
+
+
 function App() {
   return (
     <>
