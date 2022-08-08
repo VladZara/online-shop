d31838463a05b8f2efe4504a3115fef5773b0480
ArturTurcan <105306163+ArturTurcan@users.noreply.github.com>
2022-08-08 10:11:02 +0300

    add blog page(work with router)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { Route, Routes} from "react-router-dom";
 import HomePage from "./components/pages/home-page/HomePage";
 import BlogPage from "./components/pages/blog-page/BlogPage";
 import ProductPage from "./components/pages/product-page/ProductPage";
 
 const App = () => {
     return (
-        <BrowserRouter>
+        <>
             <Routes>
                 <Route path="/" element={<HomePage />}/>
                 <Route path="/product-page" element={<ProductPage />}/>
                 <Route path="/blog-page" element={<BlogPage />}/>
             </Routes>
-        </BrowserRouter>
+        </>
     );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(<App/>);
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
 
 
 
