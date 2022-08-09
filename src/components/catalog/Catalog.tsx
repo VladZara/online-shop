ce55a062fb5b880781eebc10bcbe6e250f6fb12a
vitalii <2456vitalic@gmail.com>
2022-08-09 10:41:17 +0300

    Markup Catalog

--- src/components/catalog/Catalog.tsx
+++ src/components/catalog/Catalog.tsx
@@ -4,17 +4,88 @@ import './Catalog.css'
 const Catalog = () => {
     return (
         <>
-           <div className="container-menu">
-               <ul className="turn-menu">
-                   <li>All goods<img src="./arow_img/trow.png" alt=""/></li>
-                   <li>Cleansing<img src="./arow_img/trow.png" alt=""/> </li>
-                   <li></li>
-                   <li></li>
-                   <li></li>
-                   <li></li>
-                   <li></li>
-               </ul>
-           </div>
+            <div className="container-menu">
+                <ul className="turn-menu">
+                    <li>
+                        <div className="dot-menu">
+                            <span>All goods</span>
+                            <img src="./arow_img/trow.png" alt=""/>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="dot-menu">
+                            <span>Cleansing</span>
+                            <img src="./arow_img/trow.png" alt=""/>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="dot-menu">
+                            <span> Peels and scrubs </span>
+                            <img src="./arow_img/trow.png" alt=""/>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="dot-menu">
+                            <span>Tonics</span>
+                            <img src="./arow_img/trow.png" alt=""/>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="dot-menu">
+                            <span>Creams concentrates</span>
+                            <img src="./arow_img/trow.png" alt=""/>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="dot-menu">
+                            <span>Serums</span>
+                            <img src="./arow_img/trow.png" alt=""/>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="dot-menu">
+                            <span>Essences</span>
+                            <img src="./arow_img/trow.png" alt=""/>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="dot-menu">
+                            <span>Emulsions and gels</span>
+                            <img src="./arow_img/trow.png" alt=""/>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="dot-menu">
+                            <span>Face masks</span>
+                            <img src="./arow_img/trow.png" alt=""/>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="dot-menu">
+                            <span>Pearl line</span>
+                            <img src="./arow_img/trow.png" alt=""/>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="dot-menu">
+                            <span>Body care</span>
+                            <img src="./arow_img/trow.png" alt=""/>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="dot-menu">
+                            <span>Hair care</span>
+                            <img src="./arow_img/trow.png" alt=""/>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="dot-menu">
+                            <span>Sets and accessories</span>
+                            <img src="./arow_img/trow.png" alt=""/>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </>
     );
 };
