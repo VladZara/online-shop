d2d49e7013b0f888ebee7a872b4f8c0c5561a8cc
vitalii <2456vitalic@gmail.com>
2022-08-09 09:09:44 +0300

    Inceput Catalog

---
+++ src/components/catalog/Catalog.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Catalog.css'
+
+const Catalog = () => {
+    return (
+        <>
+           <div className="container-menu">
+               <ul className="turn-menu">
+                   <li>All goods<img src="./arow_img/trow.png" alt=""/></li>
+                   <li>Cleansing<img src="./arow_img/trow.png" alt=""/> </li>
+                   <li></li>
+                   <li></li>
+                   <li></li>
+                   <li></li>
+                   <li></li>
+               </ul>
+           </div>
+        </>
+    );
+};
+
+export default Catalog;
