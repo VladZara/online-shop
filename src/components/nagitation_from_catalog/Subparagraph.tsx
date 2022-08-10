718e354f73b3db3432a91f524ef3996077b27b20
vitalii <2456vitalic@gmail.com>
2022-08-10 13:16:12 +0300

    Product with Photo

--- src/components/nagitation_from_catalog/Subparagraph.tsx
+++ src/components/nagitation_from_catalog/Subparagraph.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./Subparagraph.css"
-const Subparagraph = ({productsNames}:{productsNames: string[]}) => {
-    const fotoProduct=[{
-        title:"Callogen",
-        url: "./sub_product/callagen.png"
-    },{
-        title: "Hyaluronic",
-        url:"./sub_product/hyaluronic.png"
-    }]
+import ProducTimg from "./ProducTimg";
+const Subparagraph = ({productsNames, setSelectedPhotos}:{productsNames: string[], setSelectedPhotos: any}) => {
+    console.log(productsNames, "Parag")
+    const productsClick =(products:any)=>{
+        setSelectedPhotos(products)
+    }
     return (
         <>
             <ul className="sub-paragraph">
-                {productsNames.map((name:string) => <ul className="sub-paragraph">
-                    <li className="sub-name">{name}</li>
+                {productsNames.map(({name, products}:any) => <ul className="sub-paragraph">
+                    <li className="sub-name" onClick={()=>productsClick(products)}>{name}</li>
                 </ul>)}
             </ul>
             </>
