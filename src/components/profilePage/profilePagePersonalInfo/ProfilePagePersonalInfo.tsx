294eca0e312487f91ff1cf2187733e9373baeb85
Maxim <maximplamadeala13@gmail.com>
2022-08-11 15:50:53 +0300

    C.P.

--- src/components/profilePage/profilePagePersonalInfo/ProfilePagePersonalInfo.tsx
+++ src/components/profilePage/profilePagePersonalInfo/ProfilePagePersonalInfo.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const ProfilePagePersonalInfo = () => {
     return (
         <div className={"ProfilePagePersonalInfo"}>
-            <h4>Заполните профиль и получите 10% бонусную карту</h4>
+            <h4>Fill out your profile and get a 10% bonus card</h4>
             <div className={"inputNames"}>
                 <input placeholder={"Surname"} type="text"/>
                 <input placeholder={"Name"} type="text"/>
